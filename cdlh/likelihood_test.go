5e20b00e46e0810d394160616a58c311dd008eed
Iakov Davydov <iakov.davydov@unil.ch>
2014-08-11 18:32:38 +0200

    add logging to test

--- cdlh/likelihood_test.go
+++ cdlh/likelihood_test.go
@@ -1776,6 +1776,7 @@ func TestM0_1(tst *testing.T) {
 	cf := equalFrequency()
 	L := M0(cali, t, cf, 2, 0.5)
 	refL := -2836.196647
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
@@ -1790,6 +1791,7 @@ func TestM0_2(tst *testing.T) {
 	cf := equalFrequency()
 	L := M0(cali, t, cf, 1.79668, 0.09879)
 	refL := -1463.253413
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
@@ -1807,6 +1809,7 @@ func TestM0_3(tst *testing.T) {
 	cf := equalFrequency()
 	L := M0(cali, t, cf, 1.77621, 0.10313)
 	refL := -48631.160712
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
@@ -1824,6 +1827,7 @@ func TestH1_1(tst *testing.T) {
 	p2b := (1 - p0 - p1) * p1 / (p0 + p1)
 	L := H1(cali, t, cf, 1.909912, 0.020004, 1.000000, p0, p1, p2a, p2b)
 	refL := -2467.931313
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
@@ -1841,6 +1845,7 @@ func TestH1_2(tst *testing.T) {
 	p2b := (1 - p0 - p1) * p1 / (p0 + p1)
 	L := H1(cali, t, cf, 1.876889, 0.014541, 1.682486, p0, p1, p2a, p2b)
 	refL := -1405.850679
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
@@ -1861,6 +1866,7 @@ func TestH1_3(tst *testing.T) {
 	p2b := (1 - p0 - p1) * p1 / (p0 + p1)
 	L := H1(cali, t, cf, 1.94922, 0.072076, 13.936497, p0, p1, p2a, p2b)
 	refL := -48019.677814
+	tst.Log("L=", L, ", Ref=", refL, ", diff=", math.Abs(L-refL))
 	if math.Abs(L-refL) > smallDiff {
 		tst.Error("Expected ", refL, ", got", L)
 	}
