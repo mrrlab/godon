5d21147a3abc297a112a23359631ae9b3b6acd0a
Iakov Davydov <iakov.davydov@unil.ch>
2014-08-22 12:49:23 +0200

    constants for amcmc -> parameters

--- mcmc/amcmc.go
+++ mcmc/amcmc.go
@@ -5,16 +5,6 @@ import (
 	"math"
 )
 
-const (
-	K          = 20
-	W_SIZE     = 10
-	MAX_UPDATE = 2000
-	EPSILON2   = 5e-1
-	C          = 1
-	NU         = 3
-	LAMBDA     = 2.4
-)
-
 type Adaptive struct {
 	np         int
 	pnames     []string
@@ -29,6 +19,16 @@ type Adaptive struct {
 	delta      []bool
 	converged  []bool
 	nconverged int
+
+	// constants
+	wSize      int
+	K          int
+	Skip       int
+	MaxUpdate  int
+	Epsilon    float64
+	C          float64
+	Nu         float64
+	Lambda     float64
 }
 
 func NewAdaptive(np int, pnames []string, sd float64) (a *Adaptive) {
@@ -46,13 +46,28 @@ func NewAdaptive(np int, pnames []string, sd float64) (a *Adaptive) {
 		delta:     make([]bool, np),
 		converged: make([]bool, np),
 	}
+	a.K = 20
+	a.Skip = 2000
+	a.MaxUpdate = 2000
+	a.Epsilon = 5e-1
+	a.C = 1
+	a.Nu = 3
+	a.Lambda = 2.4
+
 	for p := 0; p < np; p++ {
 		a.mu[p] = math.NaN()
-		a.vals[p] = make(chan float64, W_SIZE)
+		a.vals[p] = make(chan float64, a.wSize)
 		a.SD[p] = sd
 	}
 
 	return
+}
+
+func (a *Adaptive) SetWSize(wSize int) {
+	a.wSize = wSize
+	for p:=0 ; p < a.np; p ++ {
+		a.vals[p] = make(chan float64, a.wSize)
+	}
 }
 
 func (a *Adaptive) RobbinsMonro(p int) (gamma, tdelta float64) {
@@ -61,13 +76,13 @@ func (a *Adaptive) RobbinsMonro(p int) (gamma, tdelta float64) {
 		a.loct[p]++
 	}
 	a.delta[p] = tdelta > 0
-	beta := 1 / math.Max(1, 1+NU)
-	gamma = C * math.Pow(float64(a.loct[p]), beta)
+	beta := 1 / math.Max(1, 1+a.Nu)
+	gamma = a.C * math.Pow(float64(a.loct[p]), beta)
 	return
 }
 
 func (a *Adaptive) CheckConvergenceMu(p int, val float64) {
-	if len(a.vals[p]) == W_SIZE {
+	if len(a.vals[p]) == a.wSize {
 		oldVal := <-a.vals[p]
 		a.sum[p] -= oldVal
 		a.sumsq[p] -= oldVal * oldVal
@@ -75,14 +90,14 @@ func (a *Adaptive) CheckConvergenceMu(p int, val float64) {
 	a.vals[p] <- val
 	a.sum[p] += val
 	a.sumsq[p] += val * val
-	if len(a.vals[p]) == W_SIZE {
+	if len(a.vals[p]) == a.wSize {
 		mean := a.sum[p] / float64(len(a.vals[p]))
 		a.SD[p] = math.Sqrt(a.sumsq[p]/float64(len(a.vals[p])) - mean*mean)
-		if a.SD[p]/mean < EPSILON2 || a.t[p]/K > MAX_UPDATE {
+		if a.SD[p]/mean < a.Epsilon || a.t[p]/a.K > a.MaxUpdate {
 			a.converged[p] = true
 			a.nconverged++
 			var reason string
-			if a.SD[p]/mean < EPSILON2 {
+			if a.SD[p]/mean < a.Epsilon {
 				reason = "SD/mean"
 				log.Print("(reason: SD/mean)")
 			} else {
@@ -102,9 +117,9 @@ func (a *Adaptive) UpdateMu(p int, val float64) {
 	}
 
 	// Recursive mu formua
-	a.bmu[p] = val/K + a.bmu[p]
+	a.bmu[p] = val/float64(a.K) + a.bmu[p]
 
-	if a.t[p] > 0 && a.t[p]%K == 0 {
+	if a.t[p] > 0 && a.t[p]%a.K == 0 {
 		gamma, delta := a.RobbinsMonro(p)
 
 		// reset batch mu
