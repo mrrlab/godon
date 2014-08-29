34b015cc7d5a6cf2dd62bdf2da0df2ae3dfcc9f4
Iakov Davydov <iakov.davydov@unil.ch>
2014-08-29 12:11:25 +0200

    add M0/mcmc chain update tests

--- cdlh/mcmcpar_test.go
+++ cdlh/mcmcpar_test.go
@@ -7,6 +7,102 @@ import (
 	"bitbucket.com/Davydov/golh/mcmc"
 )
 
+func TestBranchSiteReprM0D1(tst *testing.T) {
+	t, cali, err := GetTreeAlignment(data1)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+
+	cf := F3X4(cali)
+
+	m0 := NewM0(cali, t, cf, false)
+	chain := mcmc.NewMH(m0)
+	chain.Quiet = true
+	chain.Run(10)
+	L := chain.L
+
+	// Now reproduce
+	k, w := m0.GetParameters()
+
+	t, cali, err = GetTreeAlignment(data1)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+	cf = F3X4(cali)
+	m0 = NewM0(cali, t, cf, false)
+	m0.SetParameters(k, w)
+	newL := m0.Likelihood()
+
+	tst.Log("L=", newL, ", Ref=", L, ", diff=", math.Abs(L-newL))
+	if math.IsNaN(L) || math.Abs(L-newL) > smallDiff {
+		tst.Error("Expected ", L, ", got", newL)
+	}
+}
+
+func TestBranchSiteReprM0D2(tst *testing.T) {
+	t, cali, err := GetTreeAlignment(data2)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+
+	cf := F0()
+
+	m0 := NewM0(cali, t, cf, false)
+	chain := mcmc.NewMH(m0)
+	chain.Quiet = true
+	chain.Run(10)
+	L := chain.L
+
+	// Now reproduce
+	k, w := m0.GetParameters()
+
+	t, cali, err = GetTreeAlignment(data2)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+	cf = F0()
+	m0 = NewM0(cali, t, cf, false)
+	m0.SetParameters(k, w)
+	newL := m0.Likelihood()
+
+	tst.Log("L=", newL, ", Ref=", L, ", diff=", math.Abs(L-newL))
+	if math.IsNaN(L) || math.Abs(L-newL) > smallDiff {
+		tst.Error("Expected ", L, ", got", newL)
+	}
+}
+
+func TestBranchSiteReprM0D3(tst *testing.T) {
+	t, cali, err := GetTreeAlignment(data1)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+
+	cf := F3X4(cali)
+
+	m0 := NewM0(cali, t, cf, true)
+	chain := mcmc.NewMH(m0)
+	chain.Quiet = true
+	chain.Run(20)
+	L := chain.L
+
+	// Now reproduce
+	k, w := m0.GetParameters()
+
+	_, cali, err = GetTreeAlignment(data1)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+	cf = F3X4(cali)
+	m0 = NewM0(cali, t, cf, false)
+	m0.SetParameters(k, w)
+	newL := m0.Likelihood()
+
+	tst.Log("L=", newL, ", Ref=", L, ", diff=", math.Abs(L-newL))
+	if math.IsNaN(L) || math.Abs(L-newL) > smallDiff {
+		tst.Error("Expected ", L, ", got", newL)
+	}
+}
+
 func TestBranchSiteReprBSD1(tst *testing.T) {
 	if testing.Short() {
 		tst.Skip("skipping test in short mode.")
@@ -108,6 +204,7 @@ func TestBranchSiteReprBSD3(tst *testing.T) {
 	}
 	cf = F3X4(cali)
 	h1 = NewBranchSite(cali, t, cf, false)
+	h1.SetParameters(k, w0, w2, p0, p1)
 
 	newL := h1.Likelihood()
 
