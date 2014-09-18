86de510506068e74a5dcafb687782565418f2853
Iakov Davydov <iakov.davydov@unil.ch>
2014-09-18 12:27:47 +0200

    remove comments, remove prints, better convergence criteria for simplex

--- optimize/simplex.go
+++ optimize/simplex.go
@@ -7,6 +7,7 @@ import (
 
 const (
 	TINY        = 1e-10
+	SMALL       = 1e-6
 	SMALL_DELTA = 1.1
 )
 
@@ -27,7 +28,7 @@ type DS struct {
 func NewDS() (ds *DS) {
 	ds = &DS{
 		delta: 1,
-		ftol:  1E-10,
+		ftol:  TINY,
 	}
 	return
 }
@@ -53,7 +54,6 @@ func (ds *DS) createSimplex(opt Optimizable, delta float64) {
 			ds.l[i] = ds.points[i].Likelihood()
 		} else {
 			ds.l[i] = math.Inf(-1)
-			println("Warning: out of range", i)
 		}
 	}
 }
@@ -151,14 +151,13 @@ Iter:
 		}
 		rtol := 2 * math.Abs(ds.l[ihi]-ds.l[ilo]) / (math.Abs(ds.l[ilo]) + math.Abs(ds.l[ihi]) + TINY)
 		if rtol < ds.ftol {
-			if ds.repeat && math.Abs(ds.oldL-lhi) < 2*TINY {
+			if ds.repeat && math.Abs(ds.oldL-lhi) < SMALL {
 				break Iter
 			} else {
 				ds.repeat = true
 				ds.oldL = lhi
-				println(ds.parameters[ihi].ValuesString())
-				ds.createSimplex(ds.points[ihi], ds.delta)
 				log.Printf("converged. retrying")
+				ds.createSimplex(ds.points[ihi], ds.delta)
 				continue
 			}
 		}
@@ -175,7 +174,11 @@ Iter:
 						for j := range ds.parameters[i] {
 							ds.parameters[i][j].Set(0.5 * (ds.parameters[i][j].Get() + ds.parameters[ihi][j].Get()))
 						}
-						ds.l[i] = point.Likelihood()
+						if ds.parameters[i].InRange() {
+							ds.l[i] = point.Likelihood()
+						} else {
+							ds.l[i] = math.Inf(-1)
+						}
 					}
 
 				}
@@ -200,97 +203,3 @@ Iter:
 	}
 
 }
-
-/*
-func (ds *DS) Run() {
-
-	m.L = m.Likelihood()
-	m.MaxL = m.L
-	m.MaxPar = m.ParameterString()
-	m.PrintHeader()
-	accepted := 0
-Iter:
-	for m.i = 0; m.i < iterations; m.i++ {
-		if !m.Quiet && m.i > 0 && m.i%m.AccPeriod == 0 {
-			log.Printf("Acceptance rate %.2f%%", 100*float64(accepted)/float64(m.AccPeriod))
-			accepted = 0
-		}
-
-		if !m.Quiet && m.i%m.RepPeriod == 0 {
-			log.Printf("%d: L=%f", m.i, m.L)
-			m.PrintLine()
-		}
-		p := rand.Intn(len(m.parameters))
-		par := m.parameters[p]
-		par.Propose()
-		newL := m.Likelihood()
-
-		a := math.Exp(par.Prior() - par.OldPrior() + newL - m.L)
-		if a > 1 || rand.Float64() < a {
-			m.L = newL
-			par.Accept(m.i)
-			accepted++
-			if m.L > m.MaxL {
-				m.MaxL = m.L
-				m.MaxPar = m.ParameterString()
-			}
-		} else {
-			par.Reject()
-		}
-
-		select {
-		case s := <-m.sig:
-			log.Printf("Received signal %v, exiting.", s)
-			break Iter
-		default:
-		}
-	}
-	if !m.Quiet {
-		log.Print("Finished MCMC")
-		log.Printf("Maximum likelihood: %v", m.MaxL)
-		log.Printf("Parameter  names: %v", m.ParameterNamesString())
-		log.Printf("Parameter values: %v", m.MaxPar)
-	}
-	m.PrintFinal()
-}
-
-func (m *MH) PrintHeader() {
-	if !m.Quiet {
-		fmt.Printf("iteration\tlikelihood\t%s\n", m.ParameterNamesString())
-	}
-}
-
-func (m *MH) PrintLine() {
-	if !m.Quiet {
-		fmt.Printf("%d\t%f\t%s\n", m.i, m.L, m.ParameterString())
-	}
-}
-
-func (m *MH) PrintFinal() {
-	if !m.Quiet {
-		for _, par := range m.parameters {
-			log.Printf("%s=%v", par.Name(), par.GetValue())
-		}
-	}
-}
-
-func (m *MH) ParameterNamesString() (s string) {
-	for i, par := range m.parameters {
-		if i != 0 {
-			s += "\t"
-		}
-		s += par.Name()
-	}
-	return
-}
-
-func (m *MH) ParameterString() (s string) {
-	for i, par := range m.parameters {
-		if i != 0 {
-			s += "\t"
-		}
-		s += par.GetValue().String()
-	}
-	return
-}
-*/
