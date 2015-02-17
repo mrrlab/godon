0e1f2d9a2839ede75e8f0d10d87af3d2bf16b0c1
Iakov Davydov <iakov.davydov@unil.ch>
2015-02-17 15:21:31 +0100

    brmatch: match two trees node labels

---
+++ brmatch/brmatch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"bitbucket.com/Davydov/golh/tree"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		log.Fatal("Please provide two files")
+	}
+	in1file, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer in1file.Close()
+
+	in2file, err := os.Open(flag.Arg(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer in2file.Close()
+
+	t1, err := tree.ParseNewick(in1file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t2, err := tree.ParseNewick(in2file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nodes1 := t1.NodeIdArray()
+	nodes2 := t2.NodeIdArray()
+	if len(nodes1) != len(nodes2) {
+		log.Fatal("Two trees have different number of nodes")
+	}
+
+	for i, _ := range nodes1 {
+		nm1 := nodes1[i].Name
+		if nm1 != "" && nm1[0] == '*' {
+			nm2 := nodes2[i].Name
+			if nm2 != "" && nm2[0] == '*' {
+				fmt.Println(nm1[1:], nm2[1:])
+			}
+		}
+	}
+}
