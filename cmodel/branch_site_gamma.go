056ef26c22addc5a713e2042f2a374e09cb8d736
Iakov Davydov <iakov.davydov@unil.ch>
2016-02-06 17:17:32 +0000

    set upper boundary for w0

--- cmodel/branch_site_gamma.go
+++ cmodel/branch_site_gamma.go
@@ -119,6 +119,7 @@ func (m *BranchSiteGamma) addParameters() {
 	omega0.PriorFunc = optimize.GammaPrior(1, 2, false)
 	omega0.ProposalFunc = optimize.NormalProposal(0.01)
 	omega0.Min = 0
+	omega0.Max = 0
 	m.parameters = append(m.parameters, omega0)
 
 	if !m.fixw2 {
@@ -181,6 +182,7 @@ func (m *BranchSiteGamma) addAdaptiveParameters() {
 	}
 	omega0.PriorFunc = optimize.GammaPrior(1, 2, false)
 	omega0.Min = 0
+	omega0.Max = 1
 	m.parameters = append(m.parameters, omega0)
 
 	if !m.fixw2 {
