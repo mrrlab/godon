b7b75de335266ef411e35d87a30e94a496b365a8
Iakov Davydov <iakov.davydov@unil.ch>
2016-03-16 12:09:24 +0100

    switch to 24h buildtime

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-go install -ldflags "-X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git -C $DIR rev-parse HEAD` -X main.gitbranch=`git -C $DIR rev-parse --abbrev-ref HEAD`" bitbucket.org/Davydov/godon/godon
+go install -ldflags "-X main.buildstamp=`date -u '+%Y-%m-%d_%H:%M:%S'` -X main.githash=`git -C $DIR rev-parse HEAD` -X main.gitbranch=`git -C $DIR rev-parse --abbrev-ref HEAD`" bitbucket.org/Davydov/godon/godon
