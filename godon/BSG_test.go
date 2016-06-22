461d60a5dc64d4b5f4a26c40548ce017295124cf
Iakov Davydov <iakov.davydov@unil.ch>
2016-06-22 13:21:07 +0200

    testing two modes of BSG

--- godon/BSG_test.go
+++ godon/BSG_test.go
@@ -8,7 +8,7 @@ import (
 	"bitbucket.org/Davydov/godon/optimize"
 )
 
-func TestBSG(tst *testing.T) {
+func TestBSCG(tst *testing.T) {
 	t, cali, err := cmodel.GetTreeAlignment(data1)
 	if err != nil {
 		tst.Error("Error: ", err)
@@ -16,6 +16,38 @@ func TestBSG(tst *testing.T) {
 
 	cf := codon.F3X4(cali)
 	bsg := cmodel.NewBranchSiteGamma(cali, t, cf, true, 1, 4)
+	m := optimize.Optimizable(bsg).Copy()
+	npar := len(bsg.GetFloatParameters())
+	if npar != 5 {
+		tst.Error("Wrong number of parameters for BSG:", npar)
+	}
+
+	ds := optimize.NewDS()
+	ds.SetOptimizable(m)
+	ds.Quiet = true
+	ds.Run(5)
+
+	npar1 := len(m.GetFloatParameters())
+	m = m.Copy()
+	npar2 := len(m.GetFloatParameters())
+	if npar != npar1 || npar1 != npar2 {
+		tst.Error("Parameter number mismatch after copy:", npar, npar1, npar2)
+	}
+
+	mh := optimize.NewMH(false, 0)
+	mh.SetOptimizable(m)
+	mh.Quiet = true
+	mh.Run(5)
+}
+
+func TestBSSG(tst *testing.T) {
+	t, cali, err := cmodel.GetTreeAlignment(data1)
+	if err != nil {
+		tst.Error("Error: ", err)
+	}
+
+	cf := codon.F3X4(cali)
+	bsg := cmodel.NewBranchSiteGamma(cali, t, cf, true, 4, 1)
 	m := optimize.Optimizable(bsg).Copy()
 	npar := len(bsg.GetFloatParameters())
 	if npar != 5 {
