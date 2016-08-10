3b4bc0bf75023b4b2ec2c9b78d137b479cb678f6
Iakov Davydov <iakov.davydov@unil.ch>
2016-08-10 14:53:51 +0200

    move variables to the init file

--- codon/codon_sequences.go
+++ codon/codon_sequences.go
@@ -9,22 +9,6 @@ import (
 	"bitbucket.org/Davydov/godon/bio"
 )
 
-var (
-	// alphabet is a nucleotide alphabet.
-	alphabet = [...]byte{'T', 'C', 'A', 'G'}
-	// rAlphabet is reverse nucleotide alphabet (letter to a number)
-	rAlphabet = map[byte]byte{'T': 0, 'C': 1, 'A': 2, 'G': 3}
-	// CodonNum translates codon into its' number.
-	CodonNum = map[string]byte{}
-	// NumCodon translates codon number to its' string.
-	NumCodon = map[byte]string{}
-	// NCodon is total number of codons (61).
-	NCodon int
-	// NOCODON is a special number which represents an unknown
-	// codon.
-	NOCODON = byte(255)
-)
-
 // CodonSequence stores a sequence of codons with the sequence name.
 type CodonSequence struct {
 	Name     string
