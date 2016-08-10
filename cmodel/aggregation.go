7ed1882690c34dbde56031f924eefdb23a00ffe9
Iakov Davydov <iakov.davydov@unil.ch>
2016-08-10 13:19:39 +0200

    aggSchema type

--- cmodel/aggregation.go
+++ cmodel/aggregation.go
@@ -28,6 +28,13 @@ const (
 	AGG_RANDOM
 )
 
+// aggSchema defines an aggregation schema.
+type aggSchema struct {
+	codon2state  []int
+	state2codons [][]int
+	stateFreq    []float64
+}
+
 // observedSubL calculates likelihood for given site class and position
 // taking into account only visible states.
 func (m *BaseModel) observedSubL(class, pos int, plh [][]float64, lettersF, lettersA []int) (res float64) {
@@ -121,36 +128,39 @@ func (m *BaseModel) observedSubL(class, pos int, plh [][]float64, lettersF, lett
 
 // observedStates creates codon2state, state2codons translation slices
 // & state frequencies for observed codon-based aggregation.
-func (m *BaseModel) observedStates(lettersF, lettersA []int) (codon2state []int, state2codons [][]int, stateFreq []float64) {
+func (m *BaseModel) observedStates(lettersF, lettersA []int) (schema *aggSchema) {
 	NStates := len(lettersF)
 
-	codon2state = make([]int, codon.NCodon)
-	state2codons = make([][]int, NStates)
-	stateFreq = make([]float64, NStates)
+	schema = &aggSchema{
+		codon2state:  make([]int, codon.NCodon),
+		state2codons: make([][]int, NStates),
+		stateFreq:    make([]float64, NStates),
+	}
+
 	for i, l := range lettersF {
 		if l != codon.NCodon {
-			state2codons[i] = append(state2codons[i], l)
-			codon2state[l] = i
-			stateFreq[i] += m.cf[l]
+			schema.state2codons[i] = append(schema.state2codons[i], l)
+			schema.codon2state[l] = i
+			schema.stateFreq[i] += m.cf[l]
 		}
 	}
 	aState := NStates - 1
 	for _, l := range lettersA {
-		state2codons[aState] = append(state2codons[aState], l)
-		codon2state[l] = aState
-		stateFreq[aState] += m.cf[l]
+		schema.state2codons[aState] = append(schema.state2codons[aState], l)
+		schema.codon2state[l] = aState
+		schema.stateFreq[aState] += m.cf[l]
 	}
 	return
 }
 
 // aggSubL calculates likelihood for given site class and position
-// using provided aggregation scheme.
-func (m *BaseModel) aggSubL(class, pos int, plh [][]float64, codon2state []int, state2codons [][]int, stateFreq []float64) (res float64) {
-	NStates := len(state2codons)
+// using provided aggregation schema.
+func (m *BaseModel) aggSubL(class, pos int, plh [][]float64, schema *aggSchema) (res float64) {
+	NStates := len(schema.state2codons)
 	for node := range m.tree.Terminals() {
 		cod := m.cali[node.LeafId].Sequence[pos]
-		st := codon2state[cod]
-		for i := range state2codons {
+		st := schema.codon2state[cod]
+		for i := range schema.state2codons {
 			if cod == codon.NOCODON || i == st {
 				plh[node.Id][i] = 1
 			} else {
@@ -168,11 +178,11 @@ func (m *BaseModel) aggSubL(class, pos int, plh [][]float64, codon2state []int, 
 				s := 0.0
 				for s2 := 0; s2 < NStates; s2++ {
 					ps12 := 0.0
-					for _, l1 := range state2codons[s1] {
+					for _, l1 := range schema.state2codons[s1] {
 						// get the row
 						q := m.eQts[class][child.Id][l1*codon.NCodon:]
 						pl12 := 0.0
-						for _, l2 := range state2codons[s2] {
+						for _, l2 := range schema.state2codons[s2] {
 							pl12 += q[l2]
 
 						}
@@ -180,7 +190,7 @@ func (m *BaseModel) aggSubL(class, pos int, plh [][]float64, codon2state []int, 
 					}
 
 					//s += q.Get(l1, l2) * plh[child.Id][l2]
-					s += ps12 / stateFreq[s1] * cplh[s2]
+					s += ps12 / schema.stateFreq[s1] * cplh[s2]
 				}
 				l *= s
 			}
@@ -189,7 +199,7 @@ func (m *BaseModel) aggSubL(class, pos int, plh [][]float64, codon2state []int, 
 
 		if node.IsRoot() {
 			for s := 0; s < NStates; s++ {
-				res += stateFreq[s] * plh[node.Id][s]
+				res += schema.stateFreq[s] * plh[node.Id][s]
 			}
 			break
 		}
